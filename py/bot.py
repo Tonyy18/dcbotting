0f0cf91d6908aeeda187a54da3eba4b7dae4c919
Toni Isotalo <46251776+Tonyy18@users.noreply.github.com>
2021-03-27 18:47:01 +0200

    Update bot.py

--- py/bot.py
+++ py/bot.py
@@ -3,7 +3,7 @@ import lib
 #This file is produced by dcbotting.com engine based on your project.
 #You are free to edit and publish this file under you own license.
 
-bot = lib.Bot("NzA0NDEyMjg5NzU5MDUxOTM2.XqcxEQ.HDDq5gDtqo2kdekj4Fm2fT5VhBI")
+bot = lib.Bot("token")
 
 @bot.event
 def message_create(data):
