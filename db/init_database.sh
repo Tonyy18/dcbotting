182067886529fcab86922f3387b1249125035fac
Toni Isotalo <toni.isotalo@profitsoftware.com>
2022-11-07 12:04:42 +0200

    Ordering database scripts

--- db/init_database.sh
+++ db/init_database.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Creating database";
 mysql -u root -p < "create_database.sql";
-for filename in ./table*.sql; do
+for filename in ./*table*.sql; do
     echo "executing $filename";
     mysql -u root -p dcbotting < $filename;
 done
